8328b04bf2ad128a9a12f560ca1f86b5cb45fd55
hvu <vuhonganh91@gmail.com>
2015-11-23 08:55:32 +0000

    add: exact answer for this problem, wrongmain.cpp is the solution found on the internet and this is wrong, but however still get Accepted in the OJ, maybe because the tests do not cover all cases

--- 10249-The-Grand-Dinner/main.cpp
+++ 10249-The-Grand-Dinner/main.cpp
@@ -1,36 +1,135 @@
-#include <algorithm>
+//UVA 10249 - The Grand Dinner
+//Greedy type, seem not so difficult after sorting
+
 #include <cstdio>
+#include <algorithm>  //sort
+#include <vector>
+#include <cassert>
+#include <set>        //multiset
+#include <functional> //greater
 using namespace std;
-const int MAXT = 70;
-const int MAXN = 50;
-#define FOR(i,n) for(int i = 0; i < n; ++i)
-int cap[MAXN], tord[MAXT], ts[MAXT];
-int map[MAXT][100];
-bool ord(int a, int b){
-  return ts[a] > ts[b];
-}
-int main(void){
-  for(int m, n; scanf("%d %d", &m, &n) == 2 && (m || n); ){
-    bool possible = true;
-    FOR(i, m){ // for each team
-      scanf("%d", ts + i); // read team size
-      tord[i] = i;
-    }
-    FOR(i, n) // for each table
-      scanf("%d", cap + i); // read table capacity
-    sort(tord, tord + m, ord);
-    FOR(i, m){
-      int j, k, team = tord[i];
-      for(j = k = 0; j < n && k < ts[team]; ++j)
-	if(cap[j]){
-	  --cap[j];
-	  map[team][k++] = j + 1;
-	}
-      if(!(possible &= k == ts[team])) break;
-    }
-    puts(possible ? "1" : "0");
-    if(possible) FOR(i, m) FOR(j, ts[i])
-		   printf("%d%c", map[i][j], j == ts[i] - 1 ? 10 : 32);
+
+struct table
+{
+  int cap; //capacity of the table
+  int id;  //id of the table: to keep track after sorting
+  bool operator< (const table &a) const
+  {
+    return cap > a.cap;
   }
+};
+
+struct team 
+{
+  int id;
+  int nbMem;
+};
+
+bool sortNbMem(const team &lhs, const team &rhs)
+{
+  return lhs.nbMem > rhs.nbMem;
+}
+
+bool sortId(const team &lhs, const team &rhs)
+{
+  return lhs.id < rhs.id;
+}
+
+int main()
+{
+  
+  int nbTeam;  //number of team
+  int nbTable; //number of table
+  multiset<table> tables;
+  vector<team> teams;   //store the number of people in each team
+  while (scanf("%d %d", &nbTeam, &nbTable) == 2)
+    {
+      //INIT
+      if (nbTeam == 0 && nbTable == 0) break;
+      if (!teams.empty()) teams.clear();
+      if (!tables.empty()) tables.clear();
+
+      //READ INPUT
+      int numberMem;
+      int capacity;
+      for (int i = 0; i < nbTeam; ++i)
+	{
+	  assert(scanf("%d", &numberMem) == 1);
+	  team curTeam {i, numberMem};
+	  teams.push_back(curTeam);
+	}
+      for (int i = 0; i < nbTable; ++i)
+	{
+	  assert(scanf("%d", &capacity) == 1);
+	  table cur {capacity, i + 1};
+	  tables.insert(cur);
+	} 
+
+      //SORT (note: tables is a multiset object with operator < implemented 
+      //so it's already sorted while inserting)
+      sort(teams.begin(), teams.end(), sortNbMem);
+      
+      //COMPUTE
+      if (teams[0].nbMem > nbTable) //base case
+	{ 
+	  printf("%d\n", 0);
+	  continue;
+	}
+      int ar2D[nbTeam][teams[0].nbMem];
+      //for each team, (start with the biggest team to the smallest team)
+      //assign for each member in this team one table, start from the
+      //most available table (one has biggest available places)
+      //after assigning (done for one team), update the capacity available of 
+      //these used tables this time
+      bool possible = true;
+      vector<table> storage;
+      for (int idTeam = 0; idTeam < nbTeam && possible; ++idTeam)
+	{
+	  multiset<table>::iterator it = tables.begin();
+	  if (!storage.empty()) storage.clear();
+	  for (int idMember = 0; idMember < teams[idTeam].nbMem && possible; ++idMember)
+	    {
+	      if (it == tables.end())
+		{
+		  possible = false;
+		  break;
+		}
+	      else 
+		{
+		  ar2D[teams[idTeam].id][idMember] = (*it).id;
+		  storage.push_back(*it);		  
+		  ++it;
+		}
+	    }
+	  if (possible) //update
+	    {
+	      tables.erase(tables.begin(), it);
+	      for (int i = 0; i < teams[idTeam].nbMem; ++i)
+		{
+		  table cur = storage[i];
+		  if (cur.cap > 1)
+		    {
+		      cur.cap -= 1;
+		      tables.insert(cur);
+		    }
+		}
+	    }
+	}
+      if (!possible) printf("%d\n", 0);
+      else
+	{
+	  printf("%d\n", 1);
+	  sort(teams.begin(), teams.end(), sortId);
+	  for (int i = 0; i < nbTeam; ++i)
+	    {
+	      for (int j = 0; j < teams[i].nbMem; ++j)
+		{
+		  if (j != teams[i].nbMem - 1) printf("%d ", ar2D[i][j]);
+		  else printf("%d\n", ar2D[i][j]);
+		}
+	    }
+	}
+    }
   return 0;
 }
+
