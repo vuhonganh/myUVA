ddf285adf13143144c4a3e7050ef0e7b203cd480
hvu <vuhonganh91@gmail.com>
2015-12-10 10:06:10 +0000

    done: uva 467 Synching Signals, Accepted, should use stringstream to read input

--- 00467-Synching-Signals/main.cpp
+++ 00467-Synching-Signals/main.cpp
@@ -4,6 +4,7 @@
 #include <cassert>
 #include <vector>
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
@@ -12,20 +13,60 @@ int main()
   int periode;
   string line;
   vector<int> periodes;
+  vector<bool> green;
+  int minP;
+  int setNb = 1;
+  bool debug = false;
   while (getline(cin, line))
     {
       if (!periodes.empty()) periodes.clear();
-      cout << "printing the line: " << line << endl;
-
+      minP = 90;
       //NEED TO USE STRINGSTREAM HERE
-      while (sscanf(line.c_str(), "%d%[^\n]", &periode))
+      stringstream ss(line);
+      while (ss >> periode)
 	{
 	  periodes.push_back(periode);
+	  minP = min(periode, minP);
 	}
-      for (size_t i = 0; i < periodes.size(); ++i)
+      green.assign(periodes.size(), false);
+      if (debug) for (size_t i = 0; i < periodes.size(); ++i)
 	{
-	  cout << periodes[i] << " "; 
+	  printf((i == periodes.size() - 1)? "%d\n" : "%d ", periodes[i]);
 	}
-      cout << endl;
+      int k = 2;
+      int time = k * minP; //time for the fastest light turn green again
+      while (true)
+	{
+	  if (debug) printf("time = %d\n", time);
+	  if (time > 3600)
+	    {
+	      printf("Set %d is unable to synch after one hour.\n", setNb++);
+	      break;
+	    }
+	  size_t cnt = 0;
+	  for (size_t i = 0; i < periodes.size(); ++i)
+	    {
+	      int round = time/periodes[i];
+	      if (debug) printf("periodes[%zd] = %d, round = %d\n", i, periodes[i], round);
+	      if (round % 2 == 0) //if not red
+		{
+		  green[i] = ( time % periodes[i] < periodes[i] - 5) ? true : false; //green or yellow
+		  cnt += (green[i]) ? 1 : 0;
+		  if (debug) printf("green[%zd] = %d\n", i, (int) green[i]);
+		}
+	    }
+	  if (cnt == periodes.size()) 
+	    {
+	      printf("Set %d synchs again at %d minute(s) and %d second(s) after all turning green.\n", setNb++, time / 60, time % 60);
+	      break;
+	    }
+	  if (time < (k + 1) * minP - 5) time++;
+	  else 
+	    {
+	      k += 2;
+	      time = k * minP;
+	    }
+	}
     }
+  return 0;
 }
