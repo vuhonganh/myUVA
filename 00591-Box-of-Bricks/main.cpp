c0c5d571e5815fc3e12c649fe404b338a0cad84d
hvu <vuhonganh91@gmail.com>
2015-12-10 17:45:04 +0000

    add: uva 591 box of bricks, WIP, still Wrong Answer

---
+++ 00591-Box-of-Bricks/main.cpp
@@ -0,0 +1,36 @@
+//uva 591 box of bricks
+//simple 1D array manipulation
+
+#include <cstdio>
+#include <cassert>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+  int n;
+  vector<int> heights;
+  int setNb = 1;
+  while (scanf("%d", &n) == 1 && n != 0)
+    {
+      if (setNb != 1) printf("\n");
+      heights.assign(n, 0);
+      int sum = 0;
+      for (int i = 0; i < n; ++i)
+	{
+	  assert(scanf("%d", &heights[i]) == 1);
+	  sum += heights[i];
+	}
+      int average = sum / n;
+      int nbMove = 0;
+      for (int i = 0; i < n; ++i)
+	{
+	  nbMove += abs(average - heights[i]);
+	}
+      nbMove /= 2; //the real work is only this value
+      printf("Set #%d\nThe minimum number of moves is %d.\n", setNb++, nbMove);
+    }
+  
+  return 0;
+}
