673e8244cd6124fd3f48df7c9d38cdbccf2a4294
hvu <vuhonganh91@gmail.com>
2015-12-26 22:06:25 +0000

    modify: main.cpp in uva 790 Head Judge Headache, got Accepted, problem definition is not cleared, stupid problem that takes so much time because of its lacks of definition

--- 00790-Head-Judge-Headache/main.cpp
+++ 00790-Head-Judge-Headache/main.cpp
@@ -36,8 +36,71 @@ struct team
 
     return false;
   }
+
+  void readInput(int teamNb, char probLetter, int hour, int minute, char status)
+  {
+    m_teamNb = teamNb;
+    if (m_nbProbSol == -1) m_nbProbSol = 0; //if this is the first time this team shows off
+    int curTime = hour * 60 + minute;
+    
+    //if it's the first time read this problem for this team
+    if (m_ProbSubm[probLetter].size() == 0)
+      {
+	if (status == 'Y') //if it's correct
+	  {
+	    m_ProbSubm[probLetter].push_back(curTime);
+	  }
+	else //submission failed
+	  {
+	    m_ProbSubm[probLetter].push_back(-1); //first element is -1
+	    m_ProbSubm[probLetter].push_back(curTime); //second element is the submission time of this faillure
+	  }
+      }
+    else //contain already other information
+      {
+	if (status == 'Y')
+	  {
+	    //change if this submission is sooner or unsolved before
+	    if (m_ProbSubm[probLetter][0] == -1 || m_ProbSubm[probLetter][0] > curTime)
+	      m_ProbSubm[probLetter][0] = curTime;
+	  }
+	else //this is a unsuccessful submission, add it to the list if it's not duplicated ---- NOTE: IT COUNTS DUPLICATION so i commented the block of code below
+	  {
+	    // bool duplicate = false;
+	    // for (size_t i = 1; i < m_ProbSubm[probLetter].size(); ++i)
+	    //   {
+	    // 	if (curTime == m_ProbSubm[probLetter][i])
+	    // 	  {
+	    // 	    duplicate = true;
+	    // 	    break;
+	    // 	  }
+	    //   }
+	    // if (!duplicate) m_ProbSubm[probLetter].push_back(curTime);
+	    m_ProbSubm[probLetter].push_back(curTime); //THE SOLUTION COUNTS DUPLICATION
+	  }
+      }
+  }
   
-  void process(int teamNb, char probLetter, int hour, int minute, char status)
+  void finalProcess()
+  {
+    map<char, vector<int> >::iterator it;
+    for (it = m_ProbSubm.begin(); it != m_ProbSubm.end(); ++it)
+      {
+	vector<int> curV = it->second;
+	if ( curV[0] != -1)
+	  {
+	    for (size_t i = 1; i < curV.size(); ++i)
+	      {
+		if (curV[0] >= curV[i]) //IF IT HAPPENS AT THE SAME TIME, COUNTS IT AS HAPPENS BEFORE ALSO (that's why we use ">=" instead of ">" )
+		  m_time += 20;
+	      }
+	    m_time += curV[0];
+	    m_nbProbSol++;
+	  }
+      }
+  }
+  
+  void process(int teamNb, char probLetter, int hour, int minute, char status) //DO NOT NEED THIS ONE ANYMORE
   {
     m_teamNb = teamNb;
     if (m_nbProbSol == -1) m_nbProbSol = 0; //if this is the first time this team shows off
@@ -64,9 +127,21 @@ struct team
 	  {
 	    if (status == 'N')
 	      {
-		if (curTime < m_ProbSubm[probLetter][0]) m_time += 20;
+		if (curTime < m_ProbSubm[probLetter][0])
+		  {
+		    bool duplicate = false;
+		    for (size_t i = 1; i < m_ProbSubm[probLetter].size(); ++i)
+		      {
+			if (curTime == m_ProbSubm[probLetter][i])
+			  {
+			    duplicate = true;
+			    break;
+			  }
+		      }
+		    if (!duplicate) m_time += 20;
+		  } 
 	      }
-	    else //already solved - check which one is the sooner
+	    else //already solved and solved again - check which one is the sooner
 	      {
 		if (curTime < m_ProbSubm[probLetter][0])
 		  {
@@ -97,7 +172,7 @@ struct team
 	  }
       }
   }
-
+  
 };
 
 int main()
@@ -134,15 +209,20 @@ int main()
 	  ssToAvoidCoreDump >> teamNb >> probLetter >> hour >> twodot >> minute >> status;
 	  TRACE(teamNb); TRACE(probLetter);
 	  //cout << teamNb << probLetter << hour << twodot << minute << status;
-	  teams[teamNb].process(teamNb, probLetter, hour, minute, status);
+	  //teams[teamNb].process(teamNb, probLetter, hour, minute, status);
+	  teams[teamNb].readInput(teamNb, probLetter, hour, minute, status);
 	  teamNbMax = max(teamNb, teamNbMax);
 	}
-      for (int i = 1; i < teamNbMax; ++i)
+      for (int i = 1; i <= teamNbMax; ++i)
 	{
 	  if (teams[i].m_nbProbSol == -1) 
 	    {
 	      teams[i].m_nbProbSol = 0;
 	      teams[i].m_teamNb = i;
+	    }
+	  else //passed to readInput
+	    {
+	      teams[i].finalProcess();
 	    }
 	}
       
